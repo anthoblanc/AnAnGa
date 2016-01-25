7adea1d35602ee7af0a098d612f9dfc2e78cf399
ab <ab@web.de>
2016-01-25 16:54:25 +0100

    ABr // Acceleration_mgt // Some temporary modification for printing-purposes

--- Project_code/cps-project/code/libraries/Trajectory_management/Acceleration_mgt.cpp
+++ Project_code/cps-project/code/libraries/Trajectory_management/Acceleration_mgt.cpp
@@ -7,21 +7,22 @@ struct vector Provide_nice_curve_acc()
 	return( acc_cmd );
 }
 
-struct vector Get_Acc_straigth(struct vector velocity_refgnd, struct vector trajectory_refgnd)
+struct vector Get_Acc_straigth(const AP_HAL::HAL& hal,struct vector velocity_refgnd, struct vector trajectory_refgnd)
 {
 	struct vector acc_cmd;
+        float norm_L_squared;
+
+        norm_L_squared = 2/ScalarProduct(trajectory_refgnd,trajectory_refgnd);
 	
-	acc_cmd=CrossProduct(CrossProduct(velocity_refgnd,trajectory_refgnd),velocity_refgnd); // V*L*V
+        acc_cmd = CrossProduct(velocity_refgnd,trajectory_refgnd);
+    hal.console->printf("%f,%f,%f,%f,",acc_cmd.x,acc_cmd.y,acc_cmd.z,norm_L_squared);
+        acc_cmd=CrossProduct(acc_cmd,velocity_refgnd); // V*L*V
 	
 	//product with a scalar
-	acc_cmd.x*=2/NormVector(trajectory_refgnd)/NormVector(trajectory_refgnd); // 2/L²
-	acc_cmd.y*=2/NormVector(trajectory_refgnd)/NormVector(trajectory_refgnd); // 2/L²
-	acc_cmd.z*=2/NormVector(trajectory_refgnd)/NormVector(trajectory_refgnd); // 2/L²
+        acc_cmd.x *= norm_L_squared; // 2/L²
+        acc_cmd.y *= norm_L_squared; // 2/L²
+        acc_cmd.z *= norm_L_squared; // 2/L²
 
-	/*struct StateVariable {
-    		struct vector uvw, uvwDot ,pqr, phiThetaPsi, phiThetaPsiDot, pnPePd, pnPePdDot;
-    		float groundSpeed, groundSpeedDot;
-	};*/
 
 	return(acc_cmd);
 }
