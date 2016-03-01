7134ff3a8f3ae50de14eefad5d438e257c60b353
antho <anthony.blanc@tum.de>
2016-03-01 14:06:46 +0100

    AW//Adapter//sh file to make the operation easier

---
+++ Project_code/cps-project/Adapter_binary/old_program_adapter.sh
@@ -0,0 +1,1 @@
+/usr/bin/avrdude -D -c wiring -p atmega2560 -P /dev/ttyACM0 -b115200  -U flash:w:_old_Emulation_Adapter.hex:i
--- Project_code/cps-project/Adapter_binary/program_adapter.sh
+++ Project_code/cps-project/Adapter_binary/program_adapter.sh
@@ -1,1 +1,1 @@
-/usr/bin/avrdude -D -c wiring -p atmega2560 -P /dev/ttyACM0 -b115200  -U flash:w:_old_Emulation_Adapter.hex:i
+/usr/bin/avrdude -D -c wiring -p atmega2560 -P /dev/ttyACM0 -b115200  -U flash:w:Emulation_Adapter-with-Rudder.hex:i
